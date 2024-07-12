b06bb01e5974429c5897a0bdabddb6986a2efb37
c823g <c823@example.com>
2024-07-12 08:33:48 +0200

    Add file exclusion, notifications, and enhancements to PasterPlugin
    .gitignore:
    - Excluded files starting with an underscore (_*.*) to ignore unnecessary files.
    
    plugin.xml:
    - Updated description to use CDATA for better HTML formatting.
    - Added dependencies: com.intellij.modules.lang.
    - Added notification group for displaying notifications.
    - Enhanced action definition with keyboard shortcuts and updated action description.
    
    PasterPlugin.kt:
    - Implemented user notification after copying files to clipboard.
    - Added function to count total files including those in directories.
    - Display total file count in notification for better user feedback.
    
    These changes improve file handling, enhance user experience with notifications, and ensure better compatibility with IntelliJ IDEA modules.

--- src/main/kotlin/pkg/tool/paster/PasterPlugin.kt
+++ src/main/kotlin/pkg/tool/paster/PasterPlugin.kt
@@ -5,22 +5,47 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.notification.*
+import com.intellij.openapi.vfs.VirtualFile
 import java.awt.datatransfer.StringSelection
 
 class PasterPlugin : AnAction() {
+    companion object {
+        private val NOTIFICATION_GROUP = NotificationGroupManager.getInstance().getNotificationGroup("Paster Notifications")
+    }
     override fun actionPerformed(e: AnActionEvent) {
         val project: Project = e.project ?: return
         val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: return
+        val totalFiles = countTotalFiles(selectedFiles.toList())
 
         val processedContent = FileProcessor.processFiles(selectedFiles.toList(), project.name)
 
         // Copy the processed content to clipboard
         val selection = StringSelection(processedContent)
         CopyPasteManager.getInstance().setContents(selection)
-        // TODO: Add a notification to inform the user that the content has been copied
+        
+        // Show notification
+        NOTIFICATION_GROUP
+            .createNotification(
+                "Paster",
+                "${selectedFiles.size} item(s) copied (${totalFiles} files in total)",
+                NotificationType.INFORMATION
+            ).notify(project)
     }
 
     override fun update(e: AnActionEvent) {
         e.presentation.isEnabledAndVisible = e.project != null
+    }
+
+    private fun countTotalFiles(files: List<VirtualFile>): Int {
+        var count = 0
+        for (file in files) {
+            if (file.isDirectory) {
+                count += countTotalFiles(file.children.toList())
+            } else {
+                count++
+            }
+        }
+        return count
     }
 }
